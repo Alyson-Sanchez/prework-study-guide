f34c891fb37ec03d0bf65f576b27e6404dcaf28d
Alyson-Sanchez <coqui0429@gmail.com>
2024-02-09 19:56:36 -0500

    Merge pull request #7 from Alyson-Sanchez/feature/add-html
    Added HTML code to application

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+}
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS'){
+    console.log("Let's study CSS!");
+} else if (topic === 'JavaScript'){
+    console.log("Let's study JavaScript!");
+} else {
+    console.log("Please try again!");
+}
+*/
+// the asterisk + forward slash is called 'commenting out' a code, so it can be made inactive while still being present, in case it's needed later.
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for(let x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
+//The code above indicates that so long x is less than the value of the length of the array (4), it will continue to loop, and increase the value of x each time until it equals the value of the array
